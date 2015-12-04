20564994f01ac56137ebbb5ee8103e4d17a9d7bf
annamarie <annamarie.reyes@bicol-u.edu.ph>
2015-12-04 16:57:05 +0800

    MAIN

---
+++ main.lua
@@ -0,0 +1,32 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+_W = display.contentWidth;
+_H = display.contentHeight;
+
+local storyboard = require("storyboard");
+local sfx = require("sfx");
+--local widget = require("widget")
+	
+--display.setStatusBar(display.HiddenStatusBar);
+
+--composer.gotoScene("menu");
+
+-- local mainGroup = display.newGroup();
+
+-- local function main()
+
+-- 	mainGroup:insert(director.currentView)
+-- 	mainGroup:insert(director.nextView)
+--     director:changeScene("menu");
+-- 	return true; 
+-- end
+
+-- 
+-- forward declare the handle
+sfx.bgmusic = nil
+sfx.bgmusic = audio.loadSound("Tango.wav")
+
+storyboard.gotoScene("menu")
